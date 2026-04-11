a06c8381b59f1816a081b86524e5bd20001d78b4
fxxggah <00001106883950sp@gmail.com>
2026-04-11 12:25:51 -0300

    feat: estrutura inicial do backend da plataforma de catálogos

---
+++ src/main/java/com/catalog/CatalogPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogPlatformApplication.class, args);
+	}
+
+}
