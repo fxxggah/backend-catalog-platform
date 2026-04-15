ba6213fca3c957d5fd8faa3a88c5bb9501255471
fxxggah <00001106883950sp@gmail.com>
2026-04-15 13:56:01 -0300

    add: adiciona DTOs da entidade User

--- src/main/java/com/catalog/dto/user/UserResponse.java
+++ src/main/java/com/catalog/dto/user/UserResponse.java
@@ -1,7 +1,20 @@
 package com.catalog.dto.user;
 
+import com.catalog.domain.enums.Provider;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
 
-
-
+    private Long id;
+    private String name;
+    private String email;
+    private Provider provider;
+    private Boolean active;
+    private LocalDateTime createdAt;
 }
