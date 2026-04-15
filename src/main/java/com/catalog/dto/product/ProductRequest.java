b95bfa079122a238fac3df21009ed403819b74f1
fxxggah <00001106883950sp@gmail.com>
2026-04-15 13:58:55 -0300

    add: adiciona DTOs da entidade Product

--- src/main/java/com/catalog/dto/product/ProductRequest.java
+++ src/main/java/com/catalog/dto/product/ProductRequest.java
@@ -1,4 +1,38 @@
 package com.catalog.dto.product;
 
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRequest {
+
+    @NotBlank
+    @Size(max = 250)
+    private String name;
+
+    @NotBlank
+    @Size(max = 250)
+    private String slug;
+
+    private String description;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    @Positive
+    private BigDecimal promotionalPrice;
+
+    @NotNull
+    private Long categoryId;
+
+    @NotNull
+    private Long storeId;
+
+    private Boolean visible;
 }
--- src/main/java/com/catalog/dto/product/ProductResponse.java
+++ src/main/java/com/catalog/dto/product/ProductResponse.java
@@ -1,4 +1,31 @@
 package com.catalog.dto.product;
 
+import com.catalog.dto.productimage.ProductImageResponse;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private String slug;
+    private String description;
+
+    private BigDecimal price;
+    private BigDecimal promotionalPrice;
+
+    private Boolean visible;
+
+    private LocalDateTime createdAt;
+
+    private Long categoryId;
+
+    private List<ProductImageResponse> images;
 }
