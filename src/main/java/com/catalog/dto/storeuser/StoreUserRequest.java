cc958020be3c925f253baefda886d7165ccea278
fxxggah <00001106883950sp@gmail.com>
2026-04-15 14:01:01 -0300

    add: adiciona DTOs da entidade StoreUser

--- src/main/java/com/catalog/dto/storeuser/StoreUserRequest.java
+++
@@ -1,4 +0,0 @@
-package com.catalog.dto.storeuser;
-
-public class StoreUserRequest {
-}
--- src/main/java/com/catalog/dto/storeuser/StoreUserResponse.java
+++ src/main/java/com/catalog/dto/storeuser/StoreUserResponse.java
@@ -1,4 +1,19 @@
 package com.catalog.dto.storeuser;
 
+import com.catalog.domain.enums.Role;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StoreUserResponse {
+
+    private Long id;
+    private Long userId;
+    private Long storeId;
+    private Role role;
+    private LocalDateTime createdAt;
 }
