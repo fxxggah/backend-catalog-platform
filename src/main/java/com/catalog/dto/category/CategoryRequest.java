d71fea1014d0621a29ecbf7dcd853d95a343a37d
fxxggah <00001106883950sp@gmail.com>
2026-04-15 13:58:04 -0300

    add: adiciona DTOs da entidade Category

--- src/main/java/com/catalog/dto/category/CategoryRequest.java
+++ src/main/java/com/catalog/dto/category/CategoryRequest.java
@@ -1,4 +1,24 @@
 package com.catalog.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRequest {
+
+    @NotBlank
+    @Size(max = 250)
+    private String name;
+
+    @NotBlank
+    @Size(max = 250)
+    private String slug;
+
+    @NotNull
+    private Long storeId;
 }
--- src/main/java/com/catalog/dto/category/CategoryResponse.java
+++ src/main/java/com/catalog/dto/category/CategoryResponse.java
@@ -1,4 +1,15 @@
 package com.catalog.dto.category;
 
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryResponse {
+
+    private Long id;
+    private String name;
+    private String slug;
+    private Long storeId;
 }
