dee22fa1994f8feccb51ca8f434113a1bb86c6ff
fxxggah <00001106883950sp@gmail.com>
2026-04-15 13:59:35 -0300

    add: adiciona DTOs da entidade ProductImage

--- src/main/java/com/catalog/dto/productimage/ProductImageResponse.java
+++ src/main/java/com/catalog/dto/productimage/ProductImageResponse.java
@@ -1,4 +1,14 @@
 package com.catalog.dto.productimage;
 
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductImageResponse {
+
+    private Long id;
+    private String imageUrl;
+    private Integer position;
 }
