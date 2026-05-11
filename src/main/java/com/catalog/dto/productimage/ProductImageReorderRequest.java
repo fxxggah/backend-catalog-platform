95f56c8dd65b52eea5c81933cddd70a59dfa8532
fxxggah <00001106883950sp@gmail.com>
2026-05-11 19:08:44 -0300

    refactor: melhora o dto productimagereorderrequest

--- src/main/java/com/catalog/dto/productimage/ProductImageReorderRequest.java
+++ src/main/java/com/catalog/dto/productimage/ProductImageReorderRequest.java
@@ -1,16 +1,12 @@
 package com.catalog.dto.productimage;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class ProductImageReorderRequest {
-
-    @NotNull
-    private Long productId;
 
     @NotEmpty
     private List<Long> imageIds;
